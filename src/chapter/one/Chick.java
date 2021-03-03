7b7823eda2bfce9bd245dad39f9d4f97c2e25082
marcelo.filho <marceloasf1999@gmail.com>
2021-03-03 00:21:10 -0300

    Half of the second chapter concluded after taking the chapter 1 review exam.

--- src/chapter/one/Chick.java
+++ src/chapter/one/Chick.java
@@ -2,10 +2,14 @@ package chapter.one;
 
 public class Chick {
 
+    private void printName() {
+        System.out.println("Field 'name' has the value: " + name);
+    }
+
     private String name = "Fluffy";
     private long number = 2_147_483_648l;
 
-    { System.out.println("Setting field 'name' with value " + name); }
+    { System.out.println("Setting field 'name' with value: " + name); }
 
     public Chick() {
         name = "Tiny";
@@ -17,5 +21,7 @@ public class Chick {
 
         System.out.println(chick.name);
         System.out.println(chick.number);
+
+        chick.printName();
     }
 }
---
+++ src/chapter/two/BinaryArithmetic.java
@@ -0,0 +1,38 @@
+package chapter.two;
+
+public class BinaryArithmetic {
+
+    public static void main(String[] args) {
+
+        // Numeric promotion rules examples, this rules applies to any binary arithmetic operations (+, -, /, %).
+        
+        short a = 1;
+        short b = 2;
+
+        /* Smaller data types (byte, short and char) are promoted to int,
+        /  so the result of a + b is a int value. 
+        */
+        int c = a + b;
+
+        System.out.println(c);
+
+        short x = 14;
+        float y = 14;
+        double z = 4;
+
+        /* Promotes x to int, then promotes x to float and multiplies x and y,
+        /  then promotes x and y result (float type) to double, so it can finnaly
+        /  be divided with z, resulting in a double value.
+        */ 
+        double r = x * y / z;
+
+        System.out.println(r);
+
+        /* Obs.: Applying a unary operator (ex.: ++ or --) to a short value results in a short value,
+        /  unary operators are excluded from the third rule.
+        */
+        short u = 1;
+
+        System.out.println(++u); 
+    }
+}
