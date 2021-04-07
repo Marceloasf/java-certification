047a964fb4412f5b82a736aaabe2d2dc36ba477f
marcelo.filho <marceloasf1999@gmail.com>
2021-04-06 22:52:50 -0300

    Chapter 10 coded exception examples

---
+++ src/ocp/chapter/ten/ExceptionExamples.java
@@ -0,0 +1,54 @@
+package ocp.chapter.ten;
+
+class NoMoreCarrotsException extends Exception {
+    NoMoreCarrotsException() {
+        super("NO MORE CARROTS!");
+    }
+ }
+
+interface Animal {
+    void good() throws NoMoreCarrotsException;
+}
+
+public class ExceptionExamples implements Animal {
+
+    private String string;
+
+    public static void main(String[] args) {
+        bad();
+    }
+
+    private static void bad() throws Error {
+        try {
+            nice();
+        } catch (NoMoreCarrotsException e) {
+            System.out.println(e.getMessage());
+            System.out.println("HA NoMoreCarrots...");
+        }
+        
+        try {
+            eatCarrot();
+        } catch (NullPointerException | IllegalArgumentException | Error e) { // Handling and declaring Error and its subclasses should not be done, but it works.
+            System.out.println(e);
+            System.out.println("HA NullPointer!");
+        }
+
+        throw new Error("OH NO");
+    }
+
+    public static void nice() throws NoMoreCarrotsException {
+        System.out.println("NICE");
+
+        throw new NoMoreCarrotsException();
+    }
+
+    public void good() {
+        System.out.println("GOOD");
+    }
+
+    private static void eatCarrot() {
+        ExceptionExamples exceptionExamples = new ExceptionExamples();
+        exceptionExamples.good();
+        exceptionExamples.string.length();
+    }
+}
